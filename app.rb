d2b3faaf720b4ee4dd2bafd8ff2522b2f50318a7
Dan Hubbard <dan.hubbard@alliants.com>
2015-12-27 16:33:28 +1300

    Part 2 - passing cucumber features

--- app.rb
+++ app.rb
@@ -39,7 +39,14 @@ class HangpersonApp < Sinatra::Base
   # If a guess is invalid, set flash[:message] to "Invalid guess."
   post '/guess' do
     letter = params[:guess].to_s[0]
-    ### YOUR CODE HERE ###
+  begin
+    result = @game.guess(letter)
+  rescue ArgumentError => e
+      flash[:message] = "Invalid guess."
+  end
+    if result == false
+      flash[:message] = "You have already used that letter."
+    end
     redirect '/show'
   end
 
@@ -49,17 +56,29 @@ class HangpersonApp < Sinatra::Base
   # Notice that the show.erb template expects to use the instance variables
   # wrong_guesses and word_with_guesses from @game.
   get '/show' do
-    ### YOUR CODE HERE ###
+    result = @game.check_win_or_lose
+    case result
+    when :win
+      redirect '/win'
+    when :lose
+      redirect '/lose'
+    end
     erb :show # You may change/remove this line
   end
 
   get '/win' do
-    ### YOUR CODE HERE ###
+    unless @game.check_win_or_lose == :win
+      flash[:message] = "You can't do that!!"
+      redirect '/show'
+    end
     erb :win # You may change/remove this line
   end
 
   get '/lose' do
-    ### YOUR CODE HERE ###
+    unless @game.check_win_or_lose == :lose
+      flash[:message] = "You can't do that!!"
+      redirect '/show'
+    end
     erb :lose # You may change/remove this line
   end
 
